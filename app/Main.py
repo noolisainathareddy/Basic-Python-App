b7af41f8f2c5672ec4390e41cf6531aef33e192d
Sai natha reddy Nooli <nooli.sainathareddy@gmail.com>
2025-08-29 11:30:02 -0500

    added sp1 again

--- app/Main.py
+++ app/Main.py
@@ -11,7 +11,7 @@ def home_page():
 
 @app.get("/health")
 def app_health():
-    return 'App is up and running fine!'
+    return 'App is up and running fine! - sp1'
 
 @app.get("/project-name")
 def get_app_name():
